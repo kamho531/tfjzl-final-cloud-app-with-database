4fb1e33f4deeb1851739a01362339206b4fd96c5
kamho531 <hoky531@gmail.com>
2024-04-12 13:05:27 -0600

    update file

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-    path(route='', view=views.CourseListView.as_view(), name='index'),
+    path('', views.CourseListView.as_view(), name='index'),
     path('registration/', views.registration_request, name='registration'),
     path('login/', views.login_request, name='login'),
     path('logout/', views.logout_request, name='logout'),
