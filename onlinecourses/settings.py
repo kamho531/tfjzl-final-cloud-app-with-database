b57c330f496fcb7fe48508eb5320a7bafa5718d0
Kam Ho <hoky531@gmail.com>
2025-01-16 18:04:49 -0700

    rename myproject to onlinecourses

--- onlinecourses/settings.py
+++ onlinecourses/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myproject.urls'
+ROOT_URLCONF = 'onlinecourses.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.wsgi.application'
+WSGI_APPLICATION = 'onlinecourses.wsgi.application'
 
 
 # Database
